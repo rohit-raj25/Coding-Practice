ae3a921b0d70476fd997ece8410ffdf668804750
rohit-raj25 <rohitraj250310@gmail.com>
2022-01-07 18:15:15 +0530

    Add problem Bill Division

---
+++ basic/BillDivision.py
@@ -0,0 +1,18 @@
+# Question: Bill Division
+# Link: https://www.hackerrank.com/challenges/bon-appetit/problem
+
+def bonAppetit(bill, k, b):
+    # Write your code here
+    sum=0
+    for  i in range(len(bill)):
+        if i!=k:
+            sum+=bill[i]
+    amount=sum//2
+    
+    #print(sum)
+    if amount==b:
+        print("Bon Appetit")
+    else:
+        print(b-amount)
+
+
